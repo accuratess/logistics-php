6af4de021bf21822f03669157db4c90718cce779
HamadaEmam <hamada152emam@gmail.com>
2023-08-01 14:19:46 +0300

    fix shipmentById

--- src/Models/Filters/ShipmentById.php
+++ src/Models/Filters/ShipmentById.php
@@ -4,25 +4,7 @@ namespace Accurate\Shipping\Models\Filters;
 
 class ShipmentById
 {
-    /**
-     * 
-     *
-     * @var int $id
-     */
-    public ?int $id;
-
-    /**
-     * 
-     *
-     * @var string $code
-     */
-    public ?string $code;
-
-    public function __construct(
-        int $id = null,
-        string $code = null,
-    ) {
-        $this->id = $id;
-        $this->code = $code;
+    public function __construct(public ?int $id = null, public ?string $code = null)
+    {
     }
 }
