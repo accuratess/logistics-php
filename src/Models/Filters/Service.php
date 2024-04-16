f5070cba15bacb7707968c10db8a3f11921654ad
Hamada <hamada.emam.tech@gmail.com>
2024-04-16 14:55:11 +0200

    add from[zone-subzone] to service

--- src/Models/Filters/Service.php
+++ src/Models/Filters/Service.php
@@ -5,14 +5,30 @@ namespace Accurate\Shipping\Models\Filters;
 class Service
 {
     /**
-     * 
+     *
      *
      * @var int $serviceId
      */
     public int $serviceId;
 
-    public function __construct(int $serviceId)
+    /**
+     *
+     *
+     * @var int $fromZoneId
+     */
+    public int $fromZoneId;
+
+    /**
+     *
+     *
+     * @var int $fromSubzoneId
+     */
+    public int $fromSubzoneId;
+
+    public function __construct(int $serviceId,int $fromZoneId,int $fromSubzoneId)
     {
         $this->serviceId = $serviceId;
+        $this->fromZoneId = $fromZoneId;
+        $this->fromSubzoneId = $fromSubzoneId;
     }
 }
