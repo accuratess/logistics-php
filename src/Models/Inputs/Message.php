0bbabcf39f0b3fc1cfc14c195b8723eb7229a530
Moataz <moataz180325@fci.bu.edu.eg>
2026-03-08 14:15:09 +0200

    add remote shipment Id in create shipment message

--- src/Models/Inputs/Message.php
+++ src/Models/Inputs/Message.php
@@ -2,16 +2,28 @@
 
 namespace Accurate\Shipping\Models\Inputs;
 
+use InvalidArgumentException;
+
 class Message
 {
     public function __construct(
         public ?int $conversationId = null,
         public ?int $shipmentId = null,
+        public ?int $remoteShipmentId = null,  // Made nullable to allow one of them to be null
         public ?string $body = null,
         public ?array $images = [],
         public bool $syncIntegration = false,
     ) {
         $this->removeNullable();
+        // Validate that at least one of $id or $remoteShipmentId is provided
+        if (is_null($shipmentId) && is_null($remoteShipmentId)) {
+            throw new InvalidArgumentException('Either id or remoteShipmentId must be provided.');
+        }
+
+        // Optional: Add a check to prevent both from being set simultaneously if needed
+        if (!is_null($shipmentId) && !is_null($remoteShipmentId)) {
+            throw new InvalidArgumentException('Both id and remoteShipmentId cannot be provided together.');
+        }
     }
 
     public function removeNullable(): void
